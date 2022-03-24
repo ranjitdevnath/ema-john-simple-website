085ea226a605250515947f53b60a838208922e27
ranjitdevnath <devnathr400@gmail.com>
2022-03-24 17:36:24 +0600

    added component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './Cart.css'
+const Cart = (props) => {
+     const {cart} = props;
+     console.log(cart);
+     let total = 0;
+     let shipping = 0;
+     let quantity = 0;
+     for(const product of cart){
+          quantity = quantity + product.quantity;
+          total = total + product.price * product.quantity;
+          shipping = shipping + product.shipping;
+     }
 
-const Cart = ({cart}) => {
+     const tax = parseFloat((total * 0.1).toFixed(2));
+     const grantTotal = total + shipping + tax;
+
      return (
-          <div>
+          <div className='cart'>
                <h4>Order Summary</h4>
-               <p>Selected Items: {cart.length} </p>
+               <p>Selected Items: {quantity} </p>
+               <p>Total Price: ${total}</p>
+               <p>Shipping: ${shipping} </p>
+               <p>Tax: $ {tax} </p>
+               <h5>Grant Total: {grantTotal.toFixed(2)}</h5>
           </div>
      );
 };
